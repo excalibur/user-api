85aeab00042a0f0a33f0f0b0d3988d5cf7ee20c3
excalibur <excalibur-faith@qq.com>
2014-05-07 11:30:56 +0800

    创建结构

---
+++ core/src/main/java/ApplicationController.java
@@ -0,0 +1,9 @@
+/**
+ * [description]
+ *
+ * @author faith
+ * @since 0.0.1
+ */
+
+public class ApplicationController {
+}
---
+++ web/src/main/java/ApplicationController.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * [description]
+ *
+ * @author faith
+ * @since 0.0.1
+ */
+@WebServlet(name = "app", urlPatterns = "/")
+public class ApplicationController extends HttpServlet {
+
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("1111111");
+        resp.getWriter().println("Hello World!");
+    }
+}
